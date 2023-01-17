83a9ee647fb638f632dae2d809f38824feb78b23
Mehedi Hasan Bhuiyan <82281652+mhb98@users.noreply.github.com>
2023-01-17 16:00:36 +0600

    Update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import {WebcamImage} from 'ngx-webcam';
+import {Subject, Observable} from 'rxjs';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angularWebCam';
+title = 'gfgangularwebcam';
+
+public webcamImage: any = null;
+private trigger: Subject<void> = new Subject<void>();
+triggerSnapshot(): void {
+this.trigger.next();
+}
+handleImage(webcamImage: WebcamImage): void {
+console.info('Saved webcam image', webcamImage);
+this.webcamImage = webcamImage;
+}
+
+public get triggerObservable(): Observable<void> {
+return this.trigger.asObservable();
+}
 }
